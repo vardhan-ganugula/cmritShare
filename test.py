ea2b7e0bc951d3b3d9064fccf4fa59437bdf3650
saiteja ageera <ageerasaiteja@gmail.com>
2024-06-16 22:05:31 +0530

    finnal

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-from datetime import datetime, timedelta
-past = datetime.now() - timedelta(days=1)
-present = datetime.now()
-print(datetime(2024,6,15))
-print(past,present)
+import os
+
+
+print(os.listdir(os.path.join(os.getcwd(),"static","assignments","2197")))
